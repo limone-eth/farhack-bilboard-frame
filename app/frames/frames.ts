77a8aa2f17125cd3306f96df24d300e09ca0e6f5
lim0n3 <simonestaffa96@gmail.com>
2024-05-05 03:32:41 -0700

    add open frame xupport

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,6 +1,9 @@
 import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker";
 import { createFrames } from "frames.js/next";
 import { appURL } from "../utils";
+import { openframes } from "frames.js/middleware";
+import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
+ 
 
 type State = {
   counter: number;
@@ -14,6 +17,23 @@ export const frames = createFrames<State>({
     imagesWorkerMiddleware({
       imagesRoute: "/images",
       secret: "secret",
+    }),
+    openframes({
+      clientProtocol: {
+        id: "xmtp",
+        version: "2024-02-09",
+      },
+      handler: {
+        isValidPayload: (body: JSON) => isXmtpFrameActionPayload(body),
+        getFrameMessage: async (body: JSON) => {
+          if (!isXmtpFrameActionPayload(body)) {
+            return undefined;
+          }
+          const result = await getXmtpFrameMessage(body);
+ 
+          return { ...result };
+        },
+      },
     }),
   ],
 });
